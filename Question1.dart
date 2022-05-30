eb9536bd43e4c6d7ce696ed4d45647d7302aef90
Zamazizi  Mabe <zinhlem34@gmail.com>
2022-05-30 23:11:16 +0000

    Upload New File

---
+++ Question1.dart
@@ -0,0 +1,9 @@
+void main() {
+  var name = 'Zama';
+  var city = 'Vanderbijlpark';
+  var favApp = 'Easy Equities';
+
+  var message =
+      'My name is $name.\nI stay in $city.\nMy favourite app is $favApp.';
+  print(message);
+}
